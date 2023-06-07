2f7742cd1bcb3a1acaf1a8a4e62f5a8537485ea5
Matt <matttharma@users.noreply.github.com>
2023-06-07 19:20:10 -0400

    Update hello-world.tf
    updated with basic output.

--- hello-world.tf
+++ hello-world.tf
@@ -1,13 +1,3 @@
-terraform {
-  cloud {
-    organization = "linuxdock"
-
-    workspaces {
-      name = "matt-workspace"
-    }
-  }
-}
-
 output "hello_world" {
   value = "Hello, World from Matt Tharma!"
 }

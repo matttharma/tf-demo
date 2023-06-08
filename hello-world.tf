bf0d1b73b4a535a088ca995ea1bec9c591399579
Matt <matttharma@users.noreply.github.com>
2023-06-07 22:28:02 -0400

    Update hello-world.tf
    updated the tf org/ws info.

--- hello-world.tf
+++ hello-world.tf
@@ -1,3 +1,13 @@
+terraform {
+  cloud {
+    organization = "linuxdock"
+
+    workspaces {
+      name = "SE-Workspace"
+    }
+  }
+}
+
 output "hello_world" {
   value = "Hello, World from Matt Tharma!"
 }
